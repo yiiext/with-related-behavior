5e6335c5f63f00e9b6b72b1daffa1b5640c4d386
Alexander Kochetov <creocoder@yandex.ru>
2011-09-27 18:07:26 +0400

    codestyle fix after pull request

--- WithRelatedBehavior.php
+++ WithRelatedBehavior.php
@@ -442,12 +442,12 @@ class WithRelatedBehavior extends CActiveRecordBehavior
 		return true;
 	}
 
-    /**
-     * Save main model and all it's related models recursively.
-     * @param bool  $runValidation whether to perform validation before saving the record.
-     * @param array $data attributes and relations.
-     * @return boolean whether the saving succeeds.
-     */
+	/**
+	 * Save main model and all it's related models recursively.
+	 * @param bool  $runValidation whether to perform validation before saving the record.
+	 * @param array $data attributes and relations.
+	 * @return boolean whether the saving succeeds.
+	 */
 	public function save($runValidation=true,$data=null)
 	{
 		if(!$runValidation || $this->validate($data))
