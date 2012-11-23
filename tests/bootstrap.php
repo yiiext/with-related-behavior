5437260041750eb8da8e28b898d885de6d1cd068
resurtm <resurtm@gmail.com>
2012-11-23 19:41:06 +0600

    fixed test bootstrap to work with PHPUnit 3.7.x

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,9 @@ $config=dirname(__FILE__).'/config/test.php';
 
 require_once($yiit);
 
+// make sure non existing PHPUnit classes do not break with Yii autoloader
+Yii::$enableIncludePath = false;
+Yii::setPathOfAlias('tests', dirname(__FILE__));
+Yii::import('tests.*');
+
 Yii::createWebApplication($config);
