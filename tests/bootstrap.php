ab93cf9969acaaff2fc941585100a1b46d620c8a
resurtm <resurtm@gmail.com>
2012-11-23 19:36:29 +0600

    error_reporting bug fixed.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,4 @@
 <?php
-
-// error_reporting bug workaround https://github.com/yiiext/with-related-behavior/pull/20
-error_reporting(0);
-
 // change the following paths if necessary
 $yiit=dirname(__FILE__).'/../yii/framework/yiit.php';
 $config=dirname(__FILE__).'/config/test.php';
